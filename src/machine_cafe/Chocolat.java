119a058ae92544d0830fc8a4d544d4e352bf7c95
maxdess <maxime.dessi6@etu.univ-lorraine.fr>
2018-01-26 14:41:31 +0100

    class Ingredient et class Lait,Cafe,Sucre,Chocolat

---
+++ src/machine_cafe/Chocolat.java
@@ -0,0 +1,10 @@
+package machine_cafe;
+
+public class Chocolat extends Ingredient {
+
+	public Chocolat(int qteDepart) {
+		super(qteDepart);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/machine_cafe/Lait.java
@@ -0,0 +1,12 @@
+package machine_cafe;
+
+public class Lait extends Ingredient {
+
+	public Lait(int qteDepart) {
+		super(qteDepart);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
