7236615c0a2c6aa80d3555cc291da37390f63e9b
maxdess <maxime.dessi6@etu.univ-lorraine.fr>
2018-01-26 15:27:21 +0100

    jj

--- src/machine_cafe/MachineCafe.java
+++ src/machine_cafe/MachineCafe.java
@@ -90,6 +90,47 @@ public class MachineCafe {
 	
 	public void ajouterIngredient() {
 		
+		System.out.println("Quel ingredient voulez-vous ajouter ? ");
+		System.out.println();
+		System.out.println("1 - Cafe");
+		System.out.println("2 - lait");
+		System.out.println("3 - Chocolat");
+		System.out.println("4 - Sucre");
+		System.out.println("5 - Retour au menu");
+		System.out.println();
+		System.out.print("Votre choix : ");
+		String reponse = this.sc.nextLine();
+		System.out.println();
+		
+		int choix = -1;
+		try {
+			choix = Integer.parseInt(reponse);
+			
+			switch(choix) {
+			case 1: // acheter boisson
+				break;
+			case 2: // ajouter boisson
+				break;
+			case 3: // modifier boisson
+				break;
+			case 4: // supprimer boisson
+				break;
+			case 5: // ajouter ingredient
+				break;
+			case 6: // verifier stock
+				machine.verifierStock();
+				break;
+			case 7: // quitter
+				end = true;
+				break;
+			default : // choix incorrect
+				System.err.println("Je n'ai pas compris votre choix, veuillez réessayer.");
+				System.out.println();
+				break;
+			}
+		}
+		
+		
 	}
 	
 	public void verifierStock() {
